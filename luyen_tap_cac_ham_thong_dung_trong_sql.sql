8dc6e8a2c77376a091c767a975750baff0a72f72
vothituongvy <Vothituongvyvttv@gmail.com>
2024-01-10 20:33:02 +0700

    nop bai tap

--- luyen_tap_cac_ham_thong_dung_trong_sql.sql
+++ luyen_tap_cac_ham_thong_dung_trong_sql.sql
@@ -1,10 +1,13 @@
 use quanlysinhvien;
+-- c1
 select *
 from subject
 where Credit=(select max(Credit) from subject);
+-- c2
 select s.*, m.Mark 
 from subject s join mark m on s.Subid = m.Subid
 having m.Mark >= all (select mark from mark);
+-- c3
 select s.*,avg(m.Mark) as diem_trung_binh
 from student s
 join mark m on s.StudentId=m.StudentId
